57d565985a728666e6a7db0a78d073028721d868
Ladislav Sopko <lsopko@3di.it>
2017-07-26 12:58:14 +0200

    0.0.9-devel

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("0.0.0.1")]
 
-[assembly: AssemblyVersion("0.0.8.*")]
-[assembly: AssemblyFileVersion("0.0.8")]
-[assembly: AssemblyInformationalVersion("0.0.8-devel")]
+[assembly: AssemblyVersion("0.0.9.*")]
+[assembly: AssemblyFileVersion("0.0.9")]
+[assembly: AssemblyInformationalVersion("0.0.9-devel")]
