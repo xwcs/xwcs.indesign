355b7045fce0de82f3d7750d062208423a3cfc51
egaf-ivanleoni <ivan.leoni@egaf.it>
2020-02-25 09:59:36 +0100

    Indesign version 14

--- assets/js/id_standalone.js
+++ assets/js/id_standalone.js
@@ -7,7 +7,7 @@
  * of 0ics srls
  *
  ******************************************************************************/
-#target "indesign-13"
+#target "indesign-14"
 #targetengine "MB_BridgeRTF"
 var arguments = [
     'indesign.log',
