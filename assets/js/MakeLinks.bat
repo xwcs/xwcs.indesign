d2f2833de04cd3bbc72a0b9de44a95bd920eb657
egaf-ivanleoni <ivan.leoni@egaf.it>
2020-03-31 12:33:34 +0200

    Link creation on appdata of js files

--- assets/js/MakeLinks.bat
+++ assets/js/MakeLinks.bat
@@ -1,15 +1,15 @@
 @echo off
-rem set DEST=%appdata%\Adobe\InDesign\Version 8.0\it_IT\Scripts\Scripts Panel
-rem set DEST=C:\users\ivan.DOM_EGAF\AppData\Roaming\Adobe\InDesign\Version 8.0\it_IT\Scripts\Scripts Panel
-rem set DEST=%appdata%\Adobe\InDesign\Version 13.0\it_IT\Scripts\Scripts Panel
-rem set DEST=C:\users\ivan.DOM_EGAF\AppData\Roaming\Adobe\InDesign\Version 13.0\it_IT\Scripts\Scripts Panel
+SET DEST=%appdata%\Adobe\InDesign\Version 14.0\it_IT\Scripts\Scripts Panel
 
-set DEST=%appdata%\Adobe\InDesign\Version 14.0\it_IT\Scripts\Scripts Panel
+if NOT EXIST "%DEST%\js" MKDIR "%DEST%\js"
 
-if EXIST "%DEST%\js" goto end
-rem mklink "%DEST%\js" "%cd%" /D
-mklink "%DEST%\js" "%~dp0" /D
-:end
+FOR %%D IN (MB_BridgeRTF,xwcs.indesign,tmpl) DO (
+if NOT EXIST "%DEST%\js\%%D" mklink "%DEST%\js\%%D" "%~dp0"\%%D /D
+)
+
+FOR %%F IN (id.js, id_standalone.js, null) DO (
+if NOT EXIST "%DEST%\js\%%F" mklink "%DEST%\js\%%F" "%~dp0\%%F"
+)
 
 rem %cd% is available either to a batch file or at the command prompt and expands to the drive letter and path of the current directory (which can change e.g. by using the CD command)
 rem %~dp0 is only available within a batch file and expands to the drive letter and path in which that batch file is located (which cannot change). It is obtained from %0 which is the batch file's name.
